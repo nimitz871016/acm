18bcbf3b3c2bb38bd04d4f1827ba49e006578a35
nimitz_007 <nimitz_007@163.com>
2016-06-16 15:01:35 +0800

    1005 completed

--- poj/1005/main.cpp
+++ poj/1005/main.cpp
@@ -7,9 +7,27 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#define PI 3.1415926
+int main() {
+    int count;
+    float x,y;
+    scanf("%d",&count);
+    for(int i = 0; i < count; i++){
+        int year = 0;
+        float square = 0;
+        scanf("%f %f",&x,&y);
+        while(1){
+            //根据计算圆的半径
+            float radius = square / PI;
+            if (x*x + y*y < radius) {
+                break;
+            }else{
+                year++;
+                square+=100;
+            }
+        }
+        printf("Property %d: This property will begin eroding in year %d.\n",i + 1,year);
+    }
+    printf("END OF OUTPUT.\n");
     return 0;
 }
